3d80967e01488b3b8d0ebe2340fa0370e9328418
Johnson Lee <g.johnsonlee@gmail.com>
2015-06-27 13:35:07 +0800

    Fix bug

--- bin/autohosts.sh
+++ bin/autohosts.sh
@@ -3,9 +3,9 @@
 URL_OLD_HOSTS=/etc/hosts
 URL_NEW_HOSTS=http://googleips-google.stor.sinaapp.com/hosts
 
-if [ `grep "http://www.findspace.name" $URL_OLD_HOSTS | wc -l` -lt 0 ]; then
-    curl -sL $URL_NEW_HOSTS >> $URL_OLD_HOSTS
-else
+if [ `grep "http://www.findspace.name" $URL_OLD_HOSTS | wc -l` -gt 0 ]; then
     BEGIN=$(grep -n "^#+BEGIN" $URL_OLD_HOSTS | awk -F: '{print $1}')
-    diff -Naur <(tail -n +$BEGIN $URL_OLD_HOSTS) <(curl -sL $URL_NEW_HOSTS) | patch -p1 /etc/hosts
+    diff -Naur <(tail -n +$BEGIN $URL_OLD_HOSTS) <(curl -sL $URL_NEW_HOSTS) | patch -p1 $URL_OLD_HOSTS
+else
+    curl -sL $URL_NEW_HOSTS >> $URL_OLD_HOSTS
 fi
