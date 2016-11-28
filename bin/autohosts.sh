bfae0f11750f1d344e32a576451968c835b363b4
Johnson Lee <g.johnsonlee@gmail.com>
2016-11-28 17:27:56 +0800

    Update autohosts.sh

--- bin/autohosts.sh
+++ bin/autohosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL_OLD_HOSTS=/etc/hosts
-URL_NEW_HOSTS=http://googleips-google.stor.sinaapp.com/hosts
+URL_NEW_HOSTS=http://googlehosts-hostsfiles.stor.sinaapp.com/hosts
 
 if [ `grep "http://www.findspace.name" $URL_OLD_HOSTS | wc -l` -gt 0 ]; then
     BEGIN=$(grep -n "^#+BEGIN" $URL_OLD_HOSTS | awk -F: '{print $1}')
