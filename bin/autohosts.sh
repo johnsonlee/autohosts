81637d81319562a990a9b99fddd69b5a00c246be
Johnson Lee <g.johnsonlee@gmail.com>
2017-06-07 10:53:26 +0800

    Update autohosts.sh

--- bin/autohosts.sh
+++ bin/autohosts.sh
@@ -4,8 +4,8 @@ URL_OLD_HOSTS=/etc/hosts
 URL_NEW_HOSTS=http://googlehosts-hostsfiles.stor.sinaapp.com/hosts
 
 if [ `grep "http://www.findspace.name" $URL_OLD_HOSTS | wc -l` -gt 0 ]; then
-    BEGIN=$(grep -n "^#+BEGIN" $URL_OLD_HOSTS | awk -F: '{print $1}')
-    diff -Naur <(tail -n +$BEGIN $URL_OLD_HOSTS) <(curl -sL $URL_NEW_HOSTS) | patch -p1 $URL_OLD_HOSTS
+    UPDATE_TIME=$(grep -n "^#+UPDATE_TIME" $URL_OLD_HOSTS | awk -F: '{print $1}')
+    diff -Naur <(tail -n +$UPDATE_TIME $URL_OLD_HOSTS) <(curl -sL $URL_NEW_HOSTS) | patch -p1 $URL_OLD_HOSTS
 else
     curl -sL $URL_NEW_HOSTS >> $URL_OLD_HOSTS
 fi
